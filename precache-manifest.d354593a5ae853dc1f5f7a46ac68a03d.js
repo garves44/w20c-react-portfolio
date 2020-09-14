5d5453139a5e07250b7dde4edc1bf6e09727e648
garves44 <garves44@gmail.com>
2020-09-14 13:11:46 -0600

    Updates

---
+++ precache-manifest.d354593a5ae853dc1f5f7a46ac68a03d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a9b78fb28a82db4b79620ffa3771e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6219355882b5717c4737",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "8109054df6d14eda8db5",
+    "url": "/static/css/main.f642a819.chunk.css"
+  },
+  {
+    "revision": "6219355882b5717c4737",
+    "url": "/static/js/2.46b929cc.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/static/js/2.46b929cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8109054df6d14eda8db5",
+    "url": "/static/js/main.fafed13d.chunk.js"
+  },
+  {
+    "revision": "391cecee7a42470c68a0",
+    "url": "/static/js/runtime-main.18e29388.js"
+  }
+]);
